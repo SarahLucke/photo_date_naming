1d56a56bcc6dca41f1cd3b8af8f3e4ec36a00e2f
SaraLucke <72335980+SarahLucke@users.noreply.github.com>
2022-05-24 23:05:44 +0000

    script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # photo_date_naming
-A bash script for renaming image files according to their last modified date
+This bash script renames image files according to their last modified date.  
+When starting the script, you will be asked a few questions.  
+* 
---
+++ photo_date_naming.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+echo -n "Please enter a destination folder: "
+read destination_folder
+if ! [[ -d $destination_folder ]]; then
+	echo "This folder does not exist... exiting"
+	exit
+fi
+
+echo -n "Please enter a prefix if you wish to use one: "
+read prefix
+
+echo -n "Please enter a start date (format YYYMMDD): "
+read startdate
+while ! [[ $startdate =~ ^[0-9]{8}$ ]]; do
+	echo -n "Format is wrong... try again: "
+	read startdate
+done
+
+echo -n "If applicable, please enter an end date (format YYYMMDD): "
+read enddate
+if [[ $enddate == "" ]]; then
+	enddate=$(date +"%Y%m%d")
+	echo "-> setting to " $enddate
+fi
+
+echo -n "Please enter the destination extension: "
+read destination_extension
+if [[ $destination_extension == "" ]]; then
+	destination_extension="jpg"
+	echo "-> setting to " $destination_extension
+fi
+
+echo -n "Do you wish to delete the original files? (Y/N): "
+read delete_originals
+
+echo "### START"
+source_path=$(pwd)
+echo "scanning folder: " $source_path
+
+# counter for file names
+counter=0
+# last change:
+last_change=0
+# find all pictures in directory:
+find * -maxdepth 0 -type f -name "*" -exec file --mime-type {} \+ | awk -F: '{if ($2 ~/image\//) print $1}' | while IFS= read -r original_file; do 
+ 	# grab string containing change date from stat command:
+ 	# also: remove - and only grab date from date time string
+ 	change="$(stat --format=%y $original_file | sed -e 's/-//g' | sed -E 's|[^0-9]*([0-9]{8}).*|\1|')"
+ 	
+ 	# set parsing delimiter to space and parse change date to array:
+ 	IFS=" "
+ 	tmp_change_array=($change)
+	unset IFS
+	
+	# only process pictures in the specific time range:
+	if [[ $change -gt $startdate ]] && [[ $change -le $enddate ]]; then
+		echo "... found file: " $original_file " (" $change ")"
+		# different change date than before -> start at counter=0 again:
+		if [ $change -ne $last_change ]; then
+			last_change=$change
+			counter=0
+		fi
+		
+		# check if desired extension is allowed, if not, change it
+		possible_extensions=$(file --extension $original_file | sed -E 's|.*: ([a-zA-Z/0-9]*)|\1|')
+		local_dest_extension="${destination_extension,,}"
+		if ! grep "$local_dest_extension" <<< "$possible_extensions"; then
+			local_dest_extension="$(echo $possible_extensions | sed -E 's|^([^/]*).*|\1|')"
+			echo "setting extension to "$local_dest_extension
+		fi
+		
+
+		# check if new_filename exists, if yes, increment counter:
+		while test -e "$destination_folder"/"$change"_"$counter"."$local_dest_extension"; do
+			((counter=counter+1))
+		done
+		
+		new_filename=$prefix$change"_"$counter"."$local_dest_extension
+		echo "renaming: "$original_file" -> "$new_filename
+		# move source file to destination file name:
+		cp -i $original_file $destination_folder/$new_filename
+		
+		# catching copy error:
+		if [ $? -ne 0 ]; then
+			echo "a copy error occured - exiting"
+			exit
+		fi
+		
+		# delete file if selected:
+		if [[ $delete_originals  == "Y" ]]; then 
+			echo "deleting "$original_file
+			rm $original_file
+		fi
+		
+		# increment counter:
+		((counter=counter+1))
+	fi
+	
+done
+echo "### END"
